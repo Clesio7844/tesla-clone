8d471d241690657d069c176ba9bdc971401c9502
david <daviddecastro42@live.co.uk>
2021-01-27 23:05:39 +0000

    add header + assests + font

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className='App'>
-      <h1>hello wrold</h1>
+      <Header />
+      {/* header */}
+      {/* item => snapping */}
+      {/* item => snapping */}
+      {/* item => snapping */}
+      {/* item => snapping */}
     </div>
   );
 }
